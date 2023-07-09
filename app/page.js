fbf730f10271b625ebef870eff853f9148a1c488
Faizan <faizanalam980@gmail.com>
2023-07-09 11:41:37 +0530

    removed page.js

--- app/page.js
+++
@@ -1,9 +0,0 @@
-'use client'
-import dynamic from 'next/dynamic';
-const DynamicHome = dynamic(() => import('../components/Home'), {
-  ssr: false,
-});
-
-export default function HomePage() {
-  return <DynamicHome />;
-}
